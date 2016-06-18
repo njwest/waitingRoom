91508766865d942672b6c27fc5964d1a9f36697a
Nick West <mail@nickwe.st>
2016-06-18 18:04:03 -0400

    further styling and functionality, parallax

--- js/trainsched.js
+++ js/trainsched.js
@@ -10,7 +10,7 @@ $(function(){
   	destination = $('#destination').val().trim();
   	firstTrain = $('#firstTrain').val().trim();
   	freq = $('#freq').val().trim();
-  
+    
   awaitingRoom.push({
     'train': train,
     'destination': destination,
@@ -18,40 +18,33 @@ $(function(){
     'freq': freq
   });
 
-
   			$("#train").val('');
   			$("#destination").val('');
   			$("#firstTrain").val('');
   			$("#rate").val('');
 
   return false;
-});
+  });
+  //clearAll
+  $('#clearAll').on('click', function(){
+  	  		$("#train").val('');
+    			$("#destination").val('');
+    			$("#firstTrain").val('');
+    			$("#freq").val('');
 
-
-
-
-
-$('#clearAll').on('click', function(){
-	  		$("#train").val('');
-  			$("#destination").val('');
-  			$("#firstTrain").val('');
-  			$("#freq").val('');
-
-        return false;
-});//clearAll
+          return false;
+  });
 
 awaitingRoom.on('child_added', function(child, prevChild){
   console.log(child.val().name);
-  // for(var i = 0; i < child.length; i++){
     row = $('<tr>');
-    tTrain = $('<td>'+ child.val().train +'</td>');
-    tDestination = $('<td>'+ child.val().destination+'</td>');
-    tNextArrive = $('<td>'+ child.val().firstTrain +'</td>');
-    tMinutesAway = $('<td>'+ child.val().firstTrain +'</td>');
-    tFreq = $('<td>'+ child.val().freq+'</td>');
+      tTrain = $('<td>'+ child.val().train +'</td>');
+      tDestination = $('<td>'+ child.val().destination+ '</td>');
+      tNextArrive = $('<td>'+ child.val().firstTrain + '</td>');//add Moment.JS calculation
+      tMinutesAway = $('<td>'+ child.val().firstTrain + '</td>');//add Moment.JS calculation
+      tFreq = $('<td>'+ child.val().freq+'</td>');
     row.append(tTrain, tDestination, tFreq, tNextArrive, tMinutesAway);
-    $('tbody').append(row)
-  // }
+    $('tbody').append(row);
 })
 
 
